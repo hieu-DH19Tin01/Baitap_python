cc67c82d10913a69d74d487afa1203f30962f66a
Trí Nguyễn <minhtri17081201@gmail.com>
2021-09-17 14:39:58 +0700

    first commit

--- Baitap1_python/Baitap1_python.py
+++ Baitap1_python/Baitap1_python.py
@@ -7,17 +7,51 @@ def bai1():
                 print(x,',',end = '')
 
 # bài 2 
-print(' Nhap so can tinh giai thua :', end = '')
-n = int(input())
 def bai2():
-    x = 0
-    kq = 1
-    for x in range(n):
-        x += 1
-        kq = kq * x
-    print(' Ket qua :',kq)
-
-while n != 0:
-    bai2()
-    print(' Nhap so can tinh giai thua :',end = '')
+    print(' Nhap so can tinh giai thua :', end = '')
     n = int(input())
+    def tinh():
+        x = 0
+        kq = 1
+        for x in range(n):
+            x += 1
+            kq = kq * x
+        print(' Ket qua :',kq)
+
+    while n != 0:
+        tinh()
+        print(' Nhap so can tinh giai thua :',end = '')
+        n = int(input())
+
+# bài 3
+def bai3():
+    print(' Nhap so nguyen :', end = '')
+    n = int(input())
+    d = dict()
+    for i in range(1, n + 1):
+        d[i] = i * i
+    print(d)
+
+# bài 4
+def bai4():
+    print(' Nhap so nguyen :',end = '')
+    n = int(input())
+    l = list()
+    for i in range(n):
+        l.append(input(' Nhap so :'))
+    t = tuple(l)
+    print(l)
+    print(t)
+
+# bài 9
+import math
+def bai9():
+    import math
+    c=50
+    h=30
+    value = []
+    items=[x for x in input("Nhập giá trị của d: ").split(',')]
+    for d in items:
+        value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+    print (','.join(value)
+
